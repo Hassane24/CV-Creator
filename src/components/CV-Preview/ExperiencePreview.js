0ec4b62ea5e43325f3cce320e51da13c8b1bc535
Hassane24 <hsisane5@gmail.com>
2022-06-02 23:36:19 +0100

    exp preview

--- src/components/CV-Preview/ExperiencePreview.js
+++ src/components/CV-Preview/ExperiencePreview.js
@@ -1,5 +1,19 @@
 const ExperiencePreview = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Experience</h3>
+      <div>
+        <span>Years of experience</span>
+        <span>
+          From:{props.from} To:{props.to}
+        </span>
+      </div>
+      <div>
+        <span>Position:{props.position}</span>
+        <span>Company:{props.company}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ExperiencePreview;
