7b192dda0c37abd913a9f82f7f41a0828b306039
Hassane24 <hsisane5@gmail.com>
2022-06-05 19:22:19 +0100

    switched from class components to functional ones

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import GeneralInfo from "./CV-Form/GeneralInfo";
 import Education from "./CV-Form/Education";
 import Experience from "./CV-Form/Experience";
@@ -7,223 +7,244 @@ import "../styles/Main.css";
 import "../styles/form-styles/Form.css";
 import "../styles/preview-styles/Preview.css";
 
-class Main extends Component {
-  constructor() {
-    super();
-    this.state = {
-      renderPreview: false,
-      // General state
-      general: {
-        firstName: { text: "" },
-        lastName: { text: "" },
-        email: { text: "" },
-        number: { text: "" },
-        description: { text: "" },
-      },
-      // Education state
-      education: {
-        university: { text: "" },
-        degree: { text: "" },
-        subject: { text: "" },
-        from: { text: "" },
-        to: { text: "" },
-      },
-      // Experience state
-      experience: {
-        position: { text: "" },
-        company: { text: "" },
-        from: { text: "" },
-        to: { text: "" },
-      },
-    };
-  }
+const Main = () => {
+  const [state, setState] = useState({
+    renderPreview: false,
+  });
+
+  const [general, setGeneral] = useState({
+    firstName: { text: "" },
+    lastName: { text: "" },
+    email: { text: "" },
+    number: { text: "" },
+    description: { text: "" },
+  });
+
+  const [education, setEducation] = useState({
+    university: { text: "" },
+    degree: { text: "" },
+    subject: { text: "" },
+    from: { text: "" },
+    to: { text: "" },
+  });
+
+  const [experience, setExperience] = useState({
+    position: { text: "" },
+    company: { text: "" },
+    from: { text: "" },
+    to: { text: "" },
+  });
 
   // General section event handlers
 
-  handleFirstNameChange = (e) => {
-    this.setState((prevState) => ({
-      general: { ...prevState.general, firstName: { text: e.target.value } },
-    }));
+  const handleFirstNameChange = (e) => {
+    setGeneral((prevState) => {
+      const newGeneral = { ...prevState, firstName: { text: e.target.value } };
+      return newGeneral;
+    });
   };
 
-  handleLastNameChange = (e) => {
-    this.setState((prevState) => ({
-      general: { ...prevState.general, lastName: { text: e.target.value } },
-    }));
+  const handleLastNameChange = (e) => {
+    setGeneral((prevState) => {
+      const newGeneral = { ...prevState, lastName: { text: e.target.value } };
+      return newGeneral;
+    });
   };
 
-  handleEmailChange = (e) => {
-    this.setState((prevState) => ({
-      general: { ...prevState.general, email: { text: e.target.value } },
-    }));
+  const handleEmailChange = (e) => {
+    setGeneral((prevState) => {
+      const newGeneral = { ...prevState, email: { text: e.target.value } };
+      return newGeneral;
+    });
   };
 
-  handleNumberChange = (e) => {
-    this.setState((prevState) => ({
-      general: { ...prevState.general, number: { text: e.target.value } },
-    }));
+  const handleNumberChange = (e) => {
+    setGeneral((prevState) => {
+      const newGeneral = { ...prevState, number: { text: e.target.value } };
+      return newGeneral;
+    });
   };
 
-  handleDescriptionChange = (e) => {
-    this.setState((prevState) => ({
-      general: { ...prevState.general, description: { text: e.target.value } },
-    }));
+  const handleDescriptionChange = (e) => {
+    setGeneral((prevState) => {
+      const newGeneral = {
+        ...prevState,
+        description: { text: e.target.value },
+      };
+      return newGeneral;
+    });
   };
 
   // Education section event handlers
 
-  handleUniChange = (e) => {
-    this.setState((prevState) => ({
-      education: {
-        ...prevState.education,
+  const handleUniChange = (e) => {
+    setEducation((prevState) => {
+      const newEducation = {
+        ...prevState,
         university: { text: e.target.value },
-      },
-    }));
+      };
+      return newEducation;
+    });
   };
 
-  handleDegreeChange = (e) => {
-    this.setState((prevState) => ({
-      education: { ...prevState.education, degree: { text: e.target.value } },
-    }));
+  const handleDegreeChange = (e) => {
+    setEducation((prevState) => {
+      const newEducation = {
+        ...prevState,
+        degree: { text: e.target.value },
+      };
+      return newEducation;
+    });
   };
 
-  handleSubjectChange = (e) => {
-    this.setState((prevState) => ({
-      education: { ...prevState.education, subject: { text: e.target.value } },
-    }));
+  const handleSubjectChange = (e) => {
+    setEducation((prevState) => {
+      const newEducation = {
+        ...prevState,
+        subject: { text: e.target.value },
+      };
+      return newEducation;
+    });
   };
 
-  handleFromChange = (e) => {
-    this.setState((prevState) => ({
-      education: { ...prevState.education, from: { text: e.target.value } },
-    }));
+  const handleFromChange = (e) => {
+    setEducation((prevState) => {
+      const newEducation = {
+        ...prevState,
+        from: { text: e.target.value },
+      };
+      return newEducation;
+    });
   };
 
-  handleToChange = (e) => {
-    this.setState((prevState) => ({
-      education: { ...prevState.education, to: { text: e.target.value } },
-    }));
+  const handleToChange = (e) => {
+    setEducation((prevState) => {
+      const newEducation = {
+        ...prevState,
+        to: { text: e.target.value },
+      };
+      return newEducation;
+    });
   };
 
   // Experience section event handlers
 
-  handlePositionChange = (e) => {
-    this.setState((prevState) => ({
-      experience: {
-        ...prevState.experience,
+  const handlePositionChange = (e) => {
+    setExperience((prevState) => {
+      const newExperience = {
+        ...prevState,
         position: { text: e.target.value },
-      },
-    }));
+      };
+      return newExperience;
+    });
   };
 
-  handleCompanyChange = (e) => {
-    this.setState((prevState) => ({
-      experience: {
-        ...prevState.experience,
+  const handleCompanyChange = (e) => {
+    setExperience((prevState) => {
+      const newExperience = {
+        ...prevState,
         company: { text: e.target.value },
-      },
-    }));
+      };
+      return newExperience;
+    });
   };
 
-  handleFromExChange = (e) => {
-    this.setState((prevState) => ({
-      experience: { ...prevState.experience, from: { text: e.target.value } },
-    }));
+  const handleFromExChange = (e) => {
+    setExperience((prevState) => {
+      const newExperience = {
+        ...prevState,
+        from: { text: e.target.value },
+      };
+      return newExperience;
+    });
   };
 
-  handleToExChange = (e) => {
-    this.setState((prevState) => ({
-      experience: { ...prevState.experience, to: { text: e.target.value } },
-    }));
+  const handleToExChange = (e) => {
+    setExperience((prevState) => {
+      const newExperience = {
+        ...prevState,
+        to: { text: e.target.value },
+      };
+      return newExperience;
+    });
   };
 
   // onClick handlers
 
-  handleOnSubmit = (e) => {
+  const handleOnSubmit = (e) => {
     e.preventDefault();
-    this.setState({ renderPreview: !this.state.renderPreview });
+    setState({ renderPreview: !state.renderPreview });
   };
 
-  render() {
-    const { general, education, experience } = this.state;
-    if (this.state.renderPreview)
-      return (
-        <div className="main">
-          <Preview
+  if (state.renderPreview)
+    return (
+      <div className="main">
+        <Preview
+          firstName={general.firstName.text}
+          lastName={general.lastName.text}
+          number={general.number.text}
+          job={experience.position.text}
+          position={experience.position.text}
+          to={experience.to.text}
+          company={experience.company.text}
+          toEd={education.to.text}
+          email={general.email.text}
+          description={general.description.text}
+          fromEd={education.from.text}
+          uni={education.university.text}
+          degree={education.degree.text}
+          from={experience.from.text}
+          subject={education.subject.text}
+        ></Preview>
+        <button className="edit-btn" type="submit" onClick={handleOnSubmit}>
+          Edit your CV
+        </button>
+      </div>
+    );
+  else
+    return (
+      <div className="main">
+        <form>
+          <GeneralInfo
             firstName={general.firstName.text}
+            firstNameChange={handleFirstNameChange}
             lastName={general.lastName.text}
-            number={general.number.text}
-            job={experience.position.text}
-            position={experience.position.text}
-            to={experience.to.text}
-            company={experience.company.text}
-            toEd={education.to.text}
+            lastNameChange={handleLastNameChange}
             email={general.email.text}
+            emailChange={handleEmailChange}
+            number={general.number.text}
+            numberChange={handleNumberChange}
             description={general.description.text}
-            fromEd={education.from.text}
+            descriptionChange={handleDescriptionChange}
+          ></GeneralInfo>
+          <Education
             uni={education.university.text}
+            uniChange={handleUniChange}
             degree={education.degree.text}
-            from={experience.from.text}
+            degreeChange={handleDegreeChange}
             subject={education.subject.text}
-          ></Preview>
-          <button
-            className="edit-btn"
-            type="submit"
-            onClick={this.handleOnSubmit}
-          >
-            Edit your CV
+            subjectChange={handleSubjectChange}
+            from={education.from.text}
+            fromChange={handleFromChange}
+            to={education.to.text}
+            toChange={handleToChange}
+          ></Education>
+          <Experience
+            position={experience.position.text}
+            positionChange={handlePositionChange}
+            company={experience.company.text}
+            companyChange={handleCompanyChange}
+            from={experience.from.text}
+            fromChange={handleFromExChange}
+            to={experience.to.text}
+            toChange={handleToExChange}
+          ></Experience>
+          <button className="check-btn" type="submit" onClick={handleOnSubmit}>
+            Check your CV
           </button>
-        </div>
-      );
-    else
-      return (
-        <div className="main">
-          <form>
-            <GeneralInfo
-              firstName={general.firstName.text}
-              firstNameChange={this.handleFirstNameChange}
-              lastName={general.lastName.text}
-              lastNameChange={this.handleLastNameChange}
-              email={general.email.text}
-              emailChange={this.handleEmailChange}
-              number={general.number.text}
-              numberChange={this.handleNumberChange}
-              description={general.description.text}
-              descriptionChange={this.handleDescriptionChange}
-            ></GeneralInfo>
-            <Education
-              uni={education.university.text}
-              uniChange={this.handleUniChange}
-              degree={education.degree.text}
-              degreeChange={this.handleDegreeChange}
-              subject={education.subject.text}
-              subjectChange={this.handleSubjectChange}
-              from={education.from.text}
-              fromChange={this.handleFromChange}
-              to={education.to.text}
-              toChange={this.handleToChange}
-            ></Education>
-            <Experience
-              position={experience.position.text}
-              positionChange={this.handlePositionChange}
-              company={experience.company.text}
-              companyChange={this.handleCompanyChange}
-              from={experience.from.text}
-              fromChange={this.handleFromExChange}
-              to={experience.to.text}
-              toChange={this.handleToExChange}
-            ></Experience>
-            <button
-              className="check-btn"
-              type="submit"
-              onClick={this.handleOnSubmit}
-            >
-              Check your CV
-            </button>
-          </form>
-        </div>
-      );
-  }
-}
+        </form>
+      </div>
+    );
+};
 
 export default Main;
